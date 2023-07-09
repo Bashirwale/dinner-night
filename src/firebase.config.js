0e6cc582a6d39641b1e257e2e8d540749c8073d1
Abdullahi Bashir Wale <bashabdulbash@gmail.com>
2023-07-10 01:45:18 +0200

    added environmental variables

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_oRr-nJcBJJG6SHegaynm6mJYayhCQQM",
-  authDomain: "dinner-night-app-82867.firebaseapp.com",
-  projectId: "dinner-night-app-82867",
-  storageBucket: "dinner-night-app-82867.appspot.com",
-  messagingSenderId: "227095370438",
-  appId: "1:227095370438:web:f86119a0c39c6cd0dc15c7"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
